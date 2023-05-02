e99d9c0122576f34f912d6b223ee47e0402ca84b
Jamie Nichols <jamie337nichols@gmail.com>
2023-05-02 15:25:08 -0400

    updated

--- bin/cli.js
+++ bin/cli.js
@@ -133,6 +133,8 @@ const getEnvironments = async (sourceName, targetName) => {
 
       const [sourceConfig, targetConfig] = await getEnvironments(args.source, args.target)
       await DirectusMigrator(sourceConfig, targetConfig, args)
+    }else{
+      console.log(`No environments found. Please run ${"directus-migrator --init"} to initialize the config.``)
     }
   }
 })()
