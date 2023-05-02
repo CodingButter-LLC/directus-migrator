34c6ecb39d4f1a263087210f39578d5bffd143c9
Jamie Nichols <jamie337nichols@gmail.com>
2023-05-02 14:27:06 -0400

    update default log levell

--- src/utils/CRUD.ts
+++ src/utils/CRUD.ts
@@ -1,5 +1,5 @@
-import { Environment } from "../types/types";
-import logger from "../utils/Logger";
+import { Environment } from "../types/types"
+import logger from "../utils/Logger"
 
 export enum Method {
   GET = "GET",
@@ -11,33 +11,34 @@ export enum Method {
 
 const headers = {
   "Content-Type": "application/json",
-};
+}
 export interface CRUDInterface {
-  environment: Environment;
-  path: string;
-  data?: any;
-  handleResponse?: (response: Response) => Promise<any>;
-  params?: any;
-  method: Method;
+  environment: Environment
+  path: string
+  data?: any
+  handleResponse?: (response: Response) => Promise<any>
+  params?: any
+  method: Method
 }
 
 const URL = (environment: Environment, path: string, params: any) => {
   if (params)
     params = `&${Object.keys(params)
       .map((key) => `${key}=${params[key]}`)
-      .join("&")}`;
+      .join("&")}`
 
-  const url = `${environment.endpoint}/${path}?access_token=${
-    environment.accessToken
-  }${params ? params : ""}`;
-  return url;
-};
+  const url = `${environment.endpoint}/${path}?access_token=${environment.accessToken}${
+    params ? params : ""
+  }`
+  return url
+}
 
-export function logErrors(errors: any[]) {
+export function logErrors(errors: any[], url: string) {
+  logger.error(`Error in ${url}`)
   errors.forEach((error) => {
-    logger.error(error);
-  });
-  process.exit();
+    logger.error(error)
+  })
+  process.exit()
 }
 
 export default async function CRUD({
@@ -47,22 +48,22 @@ export default async function CRUD({
   params,
   method = Method.GET,
 }: CRUDInterface): Promise<any> {
-  const url = URL(environment, path, params);
-  logger.debug(JSON.stringify({ url, method, data }));
+  const url = URL(environment, path, params)
+  logger.debug(JSON.stringify({ url, method, data }))
   const response = await fetch(url, {
     method,
     headers,
     body: data && JSON.stringify(data),
-  });
+  })
   //check if response status is empty
-  if (response.status === 204) return false;
+  if (response.status === 204) return false
   try {
-    const json = await response.json();
+    const json = await response.json()
     if (json.errors) {
-      logErrors(json.errors);
+      logErrors(json.errors, url)
     }
-    return json;
+    return json
   } catch (e) {
-    logErrors([e]);
+    logErrors([e])
   }
 }
--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,27 +1,26 @@
-import { createLogger, format, transports, Logger, transport } from "winston";
-import { TimestampOptions } from "logform";
-import { hostname } from "os";
-import exec from "child_process";
+import { createLogger, format, transports, Logger, transport } from "winston"
+import { TimestampOptions } from "logform"
+import { hostname } from "os"
+import exec from "child_process"
 
-const { printf, combine, timestamp, label, colorize } = format;
+const { printf, combine, timestamp, label, colorize } = format
 
-let deviceLabel: string | undefined =
-  process.env.DEVICE_LABEL || process.env.NODE_ENV;
+let deviceLabel: string | undefined = process.env.DEVICE_LABEL || process.env.NODE_ENV
 
 try {
   const ghEmail: string | undefined = exec
     .execSync("git config --global user.email")
     ?.toString()
-    ?.trim();
+    ?.trim()
   const ghUser: string | undefined = exec
     .execSync("git config --global user.name")
     ?.toString()
-    ?.trim();
+    ?.trim()
   deviceLabel = ghEmail?.includes("@")
     ? `git:${ghEmail}`
     : ghUser.length
     ? `git:${ghUser}`
-    : hostname() || deviceLabel;
+    : hostname() || deviceLabel
 } catch (e) {
   // do nothing
 }
@@ -33,50 +32,44 @@ export const levels = {
   http: 3,
   verbose: 4,
   debug: 5,
-};
+}
 
 const timestampOptions: TimestampOptions =
   process.env.NODE_ENV === "development"
     ? { format: "YYYY-MM-DD HH:mm:ss" }
-    : { format: "YYYY-MM-DD" };
+    : { format: "YYYY-MM-DD" }
 
 const myFormat = printf(
-  ({ level, message, label, timestamp }) =>
-    `${timestamp} [${label}] ${level}: ${message}`
-);
+  ({ level, message, label, timestamp }) => `${timestamp} [${label}] ${level}: ${message}`
+)
 
 export class LogConfig {
-  private _label: string;
-  public logger: Logger;
-  private _format: typeof printf = printf;
-  public errorLogFile: string | undefined = process.env.ERROR_LOG_FILE;
-  public infoLogFile: string | undefined = process.env.INFO_LOG_FILE;
+  private _label: string
+  public logger: Logger
+  private _format: typeof printf = printf
+  public errorLogFile: string | undefined = process.env.ERROR_LOG_FILE
+  public infoLogFile: string | undefined = process.env.INFO_LOG_FILE
   constructor() {
-    this._label = deviceLabel || "unknown";
-    this.logger = this.createLogger();
+    this._label = deviceLabel || "unknown"
+    this.logger = this.createLogger()
   }
   setLogger(_logger: Logger) {
-    this.logger = _logger;
+    this.logger = _logger
   }
   createLogger() {
     const loggerTransports: transport[] = [
-      new transports.Console({ level: process.env.LOG_LEVEL || "debug" }),
-    ];
+      new transports.Console({ level: process.env.LOG_LEVEL || "info" }),
+    ]
 
     if (this.errorLogFile) {
-      loggerTransports.push(
-        new transports.File({ filename: "error.log", level: "error" })
-      );
+      loggerTransports.push(new transports.File({ filename: "error.log", level: "error" }))
     }
     if (this.infoLogFile) {
-      loggerTransports.push(
-        new transports.File({ filename: "info.log", level: "info" })
-      );
+      loggerTransports.push(new transports.File({ filename: "info.log", level: "info" }))
     }
-
     return createLogger({
       levels,
-      level: process.env.LOG_LEVEL || "debug",
+      level: process.env.LOG_LEVEL || "info",
       format: combine(
         colorize(),
         label({ label: this.label }),
@@ -84,27 +77,27 @@ export class LogConfig {
         myFormat
       ),
       transports: loggerTransports,
-    });
+    })
   }
 
   set format(fm: typeof printf) {
-    this._format = fm;
-    this.logger = this.createLogger();
+    this._format = fm
+    this.logger = this.createLogger()
   }
 
   get format(): typeof printf {
-    return this._format;
+    return this._format
   }
 
   get label() {
-    return this._label;
+    return this._label
   }
   set label(deviceLabel: string) {
-    this._label = deviceLabel;
-    this.logger = this.createLogger();
+    this._label = deviceLabel
+    this.logger = this.createLogger()
   }
 }
 
-const logger = new LogConfig().logger;
+const logger = new LogConfig().logger
 
-export default logger;
+export default logger
